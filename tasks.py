d191e8279eecd2344ed687253b193ec66da94e26
Feihong Hsu <feihong.hsu@gmail.com>
2016-02-04 14:24:22 -0600

    Make the index page more mobile-friendly

--- tasks.py
+++ tasks.py
@@ -4,6 +4,9 @@ from invoke import run, task
 from pathlib2 import Path, PurePath
 
 
+TITLE = 'Ionic 1 Examples'
+
+
 @task
 def serve():
     import SimpleHTTPServer
@@ -16,10 +19,14 @@ def serve():
 
 @task
 def build():
+    import string
+
     www_dirs = []
     clean()
+
     if not op.exists('build'):
         os.mkdir('build')
+
     for item in Path('.').iterdir():
         if item.is_dir():
             www_dir = item / 'www'
@@ -29,11 +36,15 @@ def build():
                 dest = Path('build') / item.name
                 print 'Copying %s to %s' % (src, dest)
                 run('cp -r %s %s' % (src, dest))
+
     with open('build/index.html', 'w') as fp:
-        fp.write('<ul>')
+        body = ''
         for www_dir in www_dirs:
-            fp.write('<li><a href="%s">%s</a></li>' % (www_dir, www_dir))
-        fp.write('</ul>')
+            body += '  <li><a href="%s">%s</a></li>\n' % (www_dir, www_dir)
+
+        tmpl = string.Template(INDEX_PAGE_TEMPLATE)
+        fp.write(tmpl.substitute(title=TITLE, body=body))
+
 
 @task
 def clean():
@@ -47,12 +58,20 @@ def publish():
     run('ghp-import -n -p build')
 
 
-def copy_or_generate(src, dest):
-    import shutil
-    if not dest.exists():
-        dest.parent.mkdir(parents=True, exist_ok=True)
-    if src.suffix == '.html':
-        with dest.open('w') as fp:
-            fp.write(generate(str(src)))
-    else:
-        shutil.copy(str(src), str(dest))
+INDEX_PAGE_TEMPLATE = """\
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<title>${title}</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<h1>${title}</h1>
+
+<ul>
+${body}
+</ul>
+</body>
+</html>"""
